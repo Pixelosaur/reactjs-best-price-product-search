1b7ee0d0e50750d96c01405c3267a465d4a5d4a4
Christina Giannouli <xristitina@gmail.com>
2020-10-14 15:13:01 +0300

    Modify App component and add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="app-wrapper">
+          <Header />
+
+            <div className="container content-wrapper">
+                <div className="row">
+                    <div className="col">
+                        I am the content
+                        {/*  Main Content Goes Here  */}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

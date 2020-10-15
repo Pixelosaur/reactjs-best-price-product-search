6540018f5437a1e284e08f725d6181907562d8e6
Christina Giannouli <xristitina@gmail.com>
2020-10-15 12:39:26 +0300

    Add Routes to App and refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+// Core
 import React from 'react';
-import './App.scss';
+// Dependencies
+import { Route, Switch } from 'react-router-dom';
+// Components
 import Header from './components/Header/Header';
+import CategoryList from './components/CategoryList/CategoryList';
+import ProductList from './components/ProductList/ProductList';
+// Styles
+import './App.scss';
 
 function App() {
     return (
         <div className="app-wrapper">
-          <Header />
-
-            <div className="container content-wrapper">
-                <div className="row">
+            <Header />
+            <div className="container">
+                <div className="row content-wrapper">
                     <div className="col">
-                        I am the content
-                        {/*  Main Content Goes Here  */}
+                        <Switch>
+                            <Route path="/" component={CategoryList} exact />
+                            <Route path="/:category/:id/products" component={ProductList} exact />
+                        </Switch>
                     </div>
                 </div>
             </div>

3476e08439d2e80eab8da4edd1916c0c39c03457
Christina Giannouli <xristitina@gmail.com>
2020-10-15 11:01:34 +0300

    Create a Constants file

---
+++ src/Constants.tsx
@@ -0,0 +1,4 @@
+const API_URL: string = `http://bp-interview.herokuapp.com`;
+
+export const API_URL_CATEGORIES: string = `${API_URL}/categories`;
+export const API_URL_PRODUCTS: string = `${API_URL}/products`;

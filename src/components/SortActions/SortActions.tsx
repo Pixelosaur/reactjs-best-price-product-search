c19804a3f45131f51aeabbe37511d20f46c2e9fe
Christina Giannouli <xristitina@gmail.com>
2020-10-16 16:18:28 +0300

    Remove state form SortActions component

--- src/components/SortActions/SortActions.tsx
+++ src/components/SortActions/SortActions.tsx
@@ -1,32 +1,23 @@
 // Core
-import React, { useState } from 'react';
+import React from 'react';
 // Dependencies
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 // Styles
 import './SortActions.scss';
 
-function SortActions({ onOrderChange }: any) {
-    const [sortButtonTitle, setSortButtonTitle] = useState<string>('Ακριβότερο');
+interface SortActionsProps {
+    onOrderChange: any;
+    sortButtonTitle: string;
+}
 
-    const handleSelect = (e: unknown) => {
-        setSortButtonTitle(e as string);
-    };
-
+function SortActions({ onOrderChange, sortButtonTitle }: SortActionsProps) {
     return (
         <DropdownButton id="dropdown-item-button" title={sortButtonTitle}>
-            <Dropdown.Item
-                eventKey="Ακριβότερο"
-                onClick={() => onOrderChange('desc')}
-                onSelect={handleSelect}
-            >
+            <Dropdown.Item eventKey="Ακριβότερο" onClick={() => onOrderChange('desc')}>
                 Ακριβότερο
             </Dropdown.Item>
-            <Dropdown.Item
-                eventKey="Φθηνότερο"
-                onClick={() => onOrderChange('asc')}
-                onSelect={handleSelect}
-            >
+            <Dropdown.Item eventKey="Φθηνότερο" onClick={() => onOrderChange('asc')}>
                 Φθηνότερο
             </Dropdown.Item>
         </DropdownButton>

4cb8d0ef49fc117c4a15a1d76cf31c220e507e11
Christina Giannouli <xristitina@gmail.com>
2020-10-16 11:09:27 +0300

    Refactor CategoryList and create a CategoryCard component

---
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -0,0 +1,36 @@
+// Core
+import React from 'react';
+// Dependencies
+import { Link } from 'react-router-dom';
+// Components
+import { Category } from '../../interfaces/Category.interface';
+// Styles
+import './CategoryCard.scss';
+
+interface CategoryCardProps<T> {
+    categories: T;
+}
+
+function CategoryCard({ categories }: CategoryCardProps<Category[]>) {
+    return (
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+            {categories?.map((category: Category, index: number) => (
+                <div key={index} className="col mb-4">
+                    <div className="card h-100">
+                        <div
+                            className="card-body d-flex flex-column justify-content-center align-items-start"
+                            style={{ backgroundImage: `url(${category.image_url})` }}
+                        >
+                            <h4 className="card-title">{category.title}</h4>
+                            <Link to={`${category.slug}/${category.id}/products`}>
+                                Δες τα προΪόντα
+                                <i className="fas fa-long-arrow-alt-right" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default CategoryCard;
--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,15 +1,13 @@
 // Core
 import React, { useEffect, useState } from 'react';
 // Dependencies
-import { Link } from 'react-router-dom';
 import axios, { AxiosResponse } from 'axios';
 // Interfaces & Constants
 import { Category } from '../../interfaces/Category.interface';
 import { API_URL_CATEGORIES } from '../../Constants';
 // Components
 import Loader from '../Loader/Loader';
-// Styles
-import './CategoryList.scss';
+import CategoryCard from '../CategoryCard/CategoryCard';
 
 function CategoryList() {
     const [categories, setCategories] = useState<Category[]>([]);
@@ -35,40 +33,7 @@ function CategoryList() {
         getCategories().then(() => setIsloading(false));
     }, [setCategories, setIsloading]);
 
-    return (
-        <>
-            {isLoading ? (
-                <Loader />
-            ) : (
-                <div className="row row-cols-1 row-cols-2">
-                    {categories.map((category: Category, index: number) => (
-                        <div key={index} className="col mb-4">
-                            <div className="card h-100">
-                                <div className="row no-gutters">
-                                    <div className="col-6">
-                                        <img
-                                            src={category.image_url}
-                                            className="card-img"
-                                            alt="Product Category"
-                                        />
-                                    </div>
-                                    <div className="col-6">
-                                        <div className="card-body">
-                                            <h5 className="card-title">{category.title}</h5>
-                                            <Link to={`${category.slug}/${category.id}/products`}>
-                                                Δες τα προΪόντα
-                                                <i className="fas fa-long-arrow-alt-right" />
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </>
-    );
+    return <>{isLoading ? <Loader /> : <CategoryCard categories={categories} />}</>;
 }
 
 export default CategoryList;
